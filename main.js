ae5b43ea24dca6a29b7b9058c073b9438f16f7d3
Peter Tascio <peter.tascio@gmail.com>
2017-07-13 18:59:43 -0400

    Add pic

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
 $('.pagination a').click(function() {
   var destId = this.attributes.href.nodeValue;
   console.log(destId);
-  
+
      $('html,body').animate({
        scrollTop: $(destId).offset().top},
        1000);
+});
+
+$('.top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000);
 });

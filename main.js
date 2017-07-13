8680a24bf709dac195eaaade6544db30e61e1e4c
Peter Tascio <peter.tascio@gmail.com>
2017-07-13 18:44:06 -0400

    Add height to info divs

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var scrollTop = $(window).scrollTop();
+
 $('.pagination a').click(function() {
-  var destId = $(this.attributes.href.nodeValue);
-  var thisTop = $(destId).offset().top;
-  var elOffset = (thisTop - scrollTop);
-  console.log(elOffset);
-  console.log(scrollTop);
-  console.log(thisTop);
-     $('html,body').animate({scrollTop: thisTop + elOffset }, 500);
+  var destId = this.attributes.href.nodeValue;
+  console.log(destId);
+  
+     $('html,body').animate({
+       scrollTop: $(destId).offset().top},
+       1000);
 });
